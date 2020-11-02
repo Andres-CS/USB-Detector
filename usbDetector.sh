22ad0d58fc9b58c1649c5b75a5fc1ba0e8057178
Andres <softwareapgm@gmail.com>
2020-11-02 00:43:44 -0500

    add 2 new functions
    read from fstab to determine default drives, and second detects a new device and logs if its added.

--- usbDetector.sh
+++ usbDetector.sh
@@ -3,6 +3,44 @@
 #Get config file
 . ./some.config
 
+declare -A uuids
+
+function readFSTAB(){
+    dfault=$(cat /etc/fstab | grep ^UUID= | cut -d " " -f 1 | cut -d "=" -f 2)
+    for a in ${dfault[@]}
+    do
+        name=$(lsblk -i -o name,uuid| grep $a | cut -d " " -f 1 | cut -d "-" -f 2)
+        uuids[$name]=$a
+    done
+}
+
+function addNewDevices(){
+    newFlag=false
+    for deviceID in $(lsblk -o uuid | grep -v "UUID")
+    do
+        for k in ${!uuids[@]}
+        do
+            if [ "${uuids[$k]}" == "$deviceID" ]
+            then
+                newFlag=false
+                break
+            else
+                newFlag=true
+            fi
+        done
+        #CHECK newFlag
+        if [ $newFlag == true ]
+        then
+            name=$(lsblk -i -o name,uuid| grep $deviceID | cut -d " " -f 1 | cut -d "-" -f 2)
+            uuids[$name]=$deviceID
+            #RESET newFlag
+            newFlag=false
+            echo "USB device $deviceID added at -> $(date)"
+            echo "USB device $deviceID added at -> $(date)" >> "/tmp/usbtracker.log"
+        fi
+    done
+}
+
 function YELLOWTAIL(){
     for uuid in $(lsblk -o uuid)
     do
@@ -25,4 +63,10 @@ function YELLOWTAIL(){
     done
 }
 
-YELLOWTAIL $myUSB
+#YELLOWTAIL $myUSB
+
+readFSTAB
+while [ true ]
+do
+    addNewDevices
+done
